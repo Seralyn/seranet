735e3d5ef8a48c0dbc6b165ef9496eb3773c3d1f
seralyn <seralyncampbell@gmail.com>
2021-10-12 10:29:00 +0900

    added placeholder html files for all links, make most links in software active, fixed sidebar menu on all pages, added content to telescopes html, testing implementation of search bar in test.html, added tooltip on software page

--- astro.js
+++ astro.js
@@ -4,20 +4,20 @@
             theme: "minimal"
         });
 
-        $('#dismiss, .overlay').on('click', function () {
-            // hide sidebar
-            $('#sidebar').removeClass('active');
-            // hide overlay
-            $('.overlay').removeClass('active');
+    $('#dismiss, .overlay').on('click', function () {
+        // hide sidebar
+        $('#sidebar').removeClass('active');
+    // hide overlay
+    $('.overlay').removeClass('active');
         });
 
-        $('#sidebarCollapse').on('click', function () {
-            // open sidebar
-            $('#sidebar').addClass('active');
-            // fade in the overlay
-            $('.overlay').addClass('active');
-            $('.collapse.in').toggleClass('in');
-            $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    $('#sidebarCollapse').on('click', function () {
+        // open sidebar
+        $('#sidebar').addClass('active');
+    // fade in the overlay
+    $('.overlay').addClass('active');
+    $('.collapse.in').toggleClass('in');
+    $('a[aria-expanded=true]').attr('aria-expanded', 'false');
         });
     });
 </script>
