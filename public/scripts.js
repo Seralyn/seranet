2e99dc5fe6d793dc1c47a80b710e592bba633808
Seralyn Campbell <seralyncampbell@gmail.com>
2022-03-30 17:01:04 +0900

    successfully animated framework section

--- public/scripts.js
+++ public/scripts.js
@@ -23,12 +23,6 @@ function init() {
       className: "seranetBG",
       duration: 4,
     });
-  // .from("#introCard", {
-  //   autoAlpha: 0,
-  //   ease: none,
-  //   xPercent: -10,
-  //   duration: 1.3,
-  // });
 
   // FADE OUT FIRST CARD
   gsap.to("#sectionOneContentContainer", {
@@ -68,91 +62,73 @@ function init() {
       markers: true,
     },
   });
+
+  // PLACE .DOCARDS UNDER 'WHAT I DO' TEXT
+  ScrollTrigger.batch(".doCard", {
+    // interval: 0.1, // time window (in seconds) for batching to occur.
+    // batchMax: 3,   // maximum batch size (targets)
+    onEnter: (batch) =>
+      gsap.from(batch, {
+        autoAlpha: 0,
+        stagger: 0.4,
+        xPercent: -10,
+        yPercent: -10,
+        duration: 0.3,
+        delay: 0.3,
+        ease: "power1.in",
+      }),
+    // also onLeave, onEnterBack, and onLeaveBack
+    // also most normal ScrollTrigger values like start, end, etc.
+  });
+
+  // PIN WEBSITE THUMBS AND THEN ENLARGE AND FADE THEM
+  ScrollTrigger.batch([".portfolioSiteThumb", ".frameworkBadge"], {
+    // interval: 0.1, // time window (in seconds) for batching to occur.
+    // batchMax: 3,   // maximum batch size (targets)
+    onEnter: (batch) =>
+      gsap.from(batch, {
+        autoAlpha: 0,
+        // xPercent: -10,
+        // scale: 3,
+        duration: 0.5,
+        delay: 0.2,
+        ease: "power1.in",
+        stagger: 0.1,
+        pin: true,
+      }),
+    // also onLeave, onEnterBack, and onLeaveBack
+    // also most normal ScrollTrigger values like start, end, etc.
+  });
+
+  // ANIMATE LANGUAGES SECTION
+  gsap.from("#sectionSixContainer .lang-card", {
+    autoAlpha: 0,
+    xPercent: -15,
+
+    scrollTrigger: {
+      trigger: "#sectionSixContainer",
+      start: "top center", //top of trigger element (#intro in this case) is at the top of the viewport
+      end: "top top",
+      scrub: true,
+      markers: true,
+      // pin: true
+    },
+  });
+
+  gsap.from("#sectionSevenContainer .frameworkLogo", {
+    autoAlpha: 0,
+    xPercent: -15,
+
+    scrollTrigger: {
+      trigger: "#sectionSevenContainer",
+      start: "top center", //top of trigger element (#intro in this case) is at the top of the viewport
+      end: "top top",
+      scrub: true,
+      markers: true,
+      // pin: true
+    },
+  });
 }
-
-// PLACE .DOCARDS UNDER 'WHAT I DO' TEXT
-ScrollTrigger.batch(".doCard", {
-  // interval: 0.1, // time window (in seconds) for batching to occur.
-  // batchMax: 3,   // maximum batch size (targets)
-  onEnter: (batch) =>
-    gsap.from(batch, {
-      autoAlpha: 0,
-      stagger: 0.4,
-      xPercent: -10,
-      yPercent: -10,
-      duration: 0.3,
-      delay: 0.5,
-      ease: "power1.in",
-    }),
-  // also onLeave, onEnterBack, and onLeaveBack
-  // also most normal ScrollTrigger values like start, end, etc.
-});
-
-// PIN WEBSITE THUMBS AND THEN ENLARGE AND FADE THEM
-ScrollTrigger.batch([".portfolioSiteThumb", ".frameworkBadge"], {
-  // interval: 0.1, // time window (in seconds) for batching to occur.
-  // batchMax: 3,   // maximum batch size (targets)
-  onEnter: (batch) =>
-    gsap.from(batch, {
-      autoAlpha: 0,
-      // xPercent: -10,
-      // scale: 3,
-      duration: 0.5,
-      delay: 1.5,
-      ease: "power1.in",
-      stagger: 0.1,
-      pin: true,
-    }),
-  // also onLeave, onEnterBack, and onLeaveBack
-  // also most normal ScrollTrigger values like start, end, etc.
-});
-
-// ANIMATE LANGUAGES SECTION
-gsap.from("#sectionSixContainer .lang-card", {
-  autoAlpha: 0,
-  xPercent: -15,
-
-  scrollTrigger: {
-    trigger: "#sectionSixContainer",
-    start: "bottom center", //top of trigger element (#intro in this case) is at the top of the viewport
-    end: "bottom top",
-    scrub: true,
-    markers: true,
-    // pin: true
-  },
-});
-
-// const doCardsThumbsTL = gsap.timeline();
-
-// doCardsThumbsTL
-//   .from("#codeCard", {
-//     ease: "power1.in",
-//     opacity: 0,
-//     duration: 0.8,
-//     xPercent: -5,
-//     stagger: 0.5,
-//   })
-//   .from("#webDesignCard", {
-//     ease: "power1.in",
-//     opacity: 0,
-//     duration: 0.8,
-//     xPercent: -5,
-//     stagger: 0.5,
-//   })
-//   .from("#castingCard", {
-//     ease: "power1.in",
-//     opacity: 0,
-//     duration: 0.8,
-//     xPercent: -5,
-//     stagger: 0.5,
-//   })
-//   .from("#photographyCard", {
-//     ease: "power1.in",
-//     opacity: 0,
-//     duration: 0.8,
-//     xPercent: -5,
-//     stagger: 0.5,
-//   });
 
 window.addEventListener("load", function () {
   init();
