26bc5bf9398dae785b65e3638c23f6bba5847837
Seralyn Campbell <seralyncampbell@gmail.com>
2022-04-04 16:41:29 +0900

    PHP form logic added, still lacks layout though

---
+++ form-to-email.php
@@ -0,0 +1,32 @@
+<?php
+    
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['descriptionMessage'];
+
+    $email_from = "seralyn.net";
+    $email_subject = "Message from Seralyn.net";
+    $email_body = 
+    "Hey, Seralyn! Somebody sent you a message from seralyn.net-\n".
+    "\n".
+    "Name: $name\n".
+    "Email: $email\n".
+    "Subject: $subject\n".
+    
+    "Message: $message";
+
+    $to = "seralyncampbell@gmail.com";
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $email \r\n";
+    
+    mail($to,$email_subject,$email_body,$headers);
+    
+    // if (mail($to, $subject, $message, $headers)) {
+    //     header('Location: thank_you.html');
+    // }
+    // else {
+    //     echo "failed";
+    // }
+    
+?>
